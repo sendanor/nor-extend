4077d3034b7d7cb6159aca85f54f4621de77198c
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-08-22 04:07:27 +0300

    Improved extend.object

--- tests/test-extend.js
+++ tests/test-extend.js
@@ -58,6 +58,38 @@ describe('extend', function(){
 			assert.strictEqual( extended_obj.foo(), 1 );
 			assert.strictEqual( extended_obj.bar(), 2 );
 		});
+
+		it('should extend ["hello", "world"] with methods from Foo (style 2)', function(){
+			function Foo() {
+			}
+			Foo.prototype.foo = function() { return 1; }
+			Foo.prototype.bar = function() { return 2; }
+			var foo = new Foo();
+			var obj = ["hello", "world"];
+			var extended_obj = extend.object(foo, Foo, obj);
+			assert.strictEqual( extended_obj[0], "hello" );
+			assert.strictEqual( extended_obj[1], "world" );
+			assert.strictEqual( extended_obj.foo(), 1 );
+			assert.strictEqual( extended_obj.bar(), 2 );
+		});
+
+		it('should extend ["hello", "world"] with methods from Foo and Bar', function(){
+			function Bar() {
+			}
+			Bar.prototype.bar = function() { return 1; }
+
+			function Foo() {
+			}
+			Foo.prototype.foo = function() { return 1; }
+			var foo = new Foo();
+			var obj = ["hello", "world"];
+			var extended_obj = extend.object(foo, [Foo, Bar], obj);
+			assert.strictEqual( extended_obj[0], "hello" );
+			assert.strictEqual( extended_obj[1], "world" );
+			assert.strictEqual( extended_obj.foo(), 1 );
+			//assert.strictEqual( extended_obj.bar(), 2 );
+		});
+
 	});
 
 	// extend.promise(methods, p)
